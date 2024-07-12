f4db9acac95dd0ccf0ba64314942d2c127b143ea
Kamburugamuwa, Pasan <pkamburu@iu.edu>
2024-07-12 10:22:30 -0400

    add configuration files

--- script.py
+++ script.py
@@ -3,8 +3,8 @@ Purpose:
     The script is used to go through each of the Mastodon files and
 
 Inputs:
-    None
-
+    - MBFC List
+    - Streamer files
 Output:
     None
 
@@ -13,8 +13,9 @@ Author: Pasan Kamburugamuwa
 
 
 import pandas as pd
-import gzip, os, re, json
+import gzip, os, re
 import yaml
+import logging
 
 #Read the MBFC list and store the data
 df = pd.read_excel("MBFC List.xlsx")
@@ -25,14 +26,35 @@ MBFC_SET = set(df['actual_URL'])
 with open('config.yml') as f:
     config = yaml.safe_load(f)
 
+#loggers
+log_dir = config['LOG_DIR']
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
+log_dir_path = os.path.join(log_dir, 'extracting_posts.log')
+logging.basicConfig(filename=log_dir_path, level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s:%(message)s')
+
 #Clean the Urls
 def clean_urls(url):
+    """
+        Clean the MBFC urls
+    :param :
+        MBFC url
+    :return:
+        cleaned url
+    """
     clean_url = re.sub(r'^https?://(www\.)?', '', url)
     # Remove any trailing slash
     clean_url = clean_url.rstrip('/')
     return clean_url
 
 def make_common(file_name):
+    """
+        Get the common file name for each Mastodon instance
+    :param file_name:
+    :return:
+    """
     match = re.match(r'([^_]*)_', file_name)
     if match:
         return match.group(1)
@@ -43,7 +65,6 @@ df['cleaned_URL'] = df['actual_URL'].apply(clean_urls)
 # Convert the cleaned URLs to a set
 MBFC_SET = set(df['cleaned_URL'])
 
-print(MBFC_SET)
 # #Iterate through each file in the folder
 for month in config['MONTHS']:
     #Get the month folder
@@ -57,6 +78,7 @@ for month in config['MONTHS']:
             for file in os.listdir(dates_files):
                 lines_to_write = []
                 file_path = os.path.join(dates_files, file)
+                logging.info(f"Getting the file to read - {file_path}")
                 with gzip.open(file_path, 'rt') as gzip_file:
                     for line in gzip_file:
                         included_mbfc = False
@@ -73,8 +95,13 @@ for month in config['MONTHS']:
 
                 # Create the directory if they not exist
                 os.makedirs(os.path.dirname(extracted_data_file_path), exist_ok=True)
-                with open(extracted_data_file_path, 'w') as f:
+                with open(extracted_data_file_path, 'a') as f:
                     f.writelines(lines_to_write)
+                logging.info(f"Successfully extracted MBFC posts from the file - {file_path}")
+
+
+
+
 
 
 
